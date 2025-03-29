7c8d5b6abf6da4d2d3b400b2cb2f548e4c6103bb
Arman-Kabir <armankabir8@gmail.com>
2025-03-29 16:47:05 +0600

    main components done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+import { useState } from 'react'
 import './App.css'
+import Button from './components/Button/Button';
 
 function App() {
+  const [checked,setChecked] = useState(false);
+  console.log(checked);
   return (
-    <>
+    <div className='frame'>
+      <div className="card">
+        <div className="text-box">
+          <p>All pages</p>
 
-    </>
+          <label className="checkbox-container">
+            <input type="checkbox"
+            checked={checked}
+            onChange={()=>setChecked(!checked)}
+            ></input>
+            
+            <span className="checkmark"></span>
+          </label>
+
+        </div>
+        <div className="divider"></div>
+        <div className="divider"></div>
+       <Button></Button>
+      </div>
+
+    </div>
   )
 }
 
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+
+const Button = () => {
+  return (
+    <div>
+        <button className="btn">Done</button>
+    </div>
+  )
+}
+
+export default Button
